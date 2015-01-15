b1ebb78b5d7fbe89b6d9658625cdf59ac0c1b68e
Chris Memoli <christophermemoli@christopher-memolis-MacBook-Pro.local>
2015-01-15 14:17:24 -0500

    css/style.css: Give left-container max/min-width of 250px, give footerlink1 margins of 1%. index.html: Move slider javascript to separate file & referenced it, and move facebook like button below twitter link. index3.html: same as index.html. js/slider.js: Result of moving javascript from html file to separate file

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,22 @@
-
-/*
-JavaScript Image slideshow:
-*/
-
-var slideimages=new Array()
-var slidelinks=new Array()
+/*JavaScript Image slideshow:*/
+var slideimages = new Array();
+var slidelinks = new Array();
 function slideshowimages(){
-for (i=0;i<slideshowimages.arguments.length;i++){
-slideimages[i]=new Image()
-slideimages[i].src=slideshowimages.arguments[i]
+	for (i = 0; i < slideshowimages.arguments.length; i++){
+		slideimages[i] = new Image();
+		slideimages[i].src=slideshowimages.arguments[i];
+	}
 }
-}
-
 function slideshowlinks(){
-for (i=0;i<slideshowlinks.arguments.length;i++)
-slidelinks[i]=slideshowlinks.arguments[i]
+	for (i = 0; i < slideshowlinks.arguments.length; i++){
+		slidelinks[i]=slideshowlinks.arguments[i];
+	}
 }
-
 function gotoshow(){
-if (!window.winslide||winslide.closed)
-winslide=window.open(slidelinks[whichlink])
-else
-winslide.location=slidelinks[whichlink]
-winslide.focus()
+	if (!window.winslide||winslide.closed){
+		winslide=window.open(slidelinks[whichlink]);
+	}else{
+		winslide.location=slidelinks[whichlink];
+		winslide.focus();
+	}
 }
-
-//-->
