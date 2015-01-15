c7a28224f2b751ad9ee8715d277baf49ad425fed
Chris Memoli <christophermemoli@christopher-memolis-MacBook-Pro.local>
2015-01-15 13:03:43 -0500

    FaceBookComments: Add new file, FacebookComments. index.html: reindent navbar. index2.html: delete. index3.html: move javascript slider from inside the page to separate file. js/slider.js: Create slider.js file for index3.html slider javascript

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+
+/*
+JavaScript Image slideshow:
+*/
+
+var slideimages=new Array()
+var slidelinks=new Array()
+function slideshowimages(){
+for (i=0;i<slideshowimages.arguments.length;i++){
+slideimages[i]=new Image()
+slideimages[i].src=slideshowimages.arguments[i]
+}
+}
+
+function slideshowlinks(){
+for (i=0;i<slideshowlinks.arguments.length;i++)
+slidelinks[i]=slideshowlinks.arguments[i]
+}
+
+function gotoshow(){
+if (!window.winslide||winslide.closed)
+winslide=window.open(slidelinks[whichlink])
+else
+winslide.location=slidelinks[whichlink]
+winslide.focus()
+}
+
+//-->
