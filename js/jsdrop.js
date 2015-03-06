eec887403c011376927c65f698896c0f809e9816
Chris Memoli <christophermemoli@christopher-memolis-MacBook-Pro.local>
2015-03-05 21:09:44 -0500

    i added a new slideshow and javascript file to move the slideshow down when the menu bar expands down

---
+++ js/jsdrop.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+        $('#cssdropdown li.headlink').hover(
+            function() { $('ul', this).css('display', 'block'); },
+            function() { $('ul', this).css('display', 'none'); });
+    });
